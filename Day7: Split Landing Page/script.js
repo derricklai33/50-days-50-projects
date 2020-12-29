c7958c4d8395043f8547524a41d64f110f9e3546
derricklai33 <derricklai33@hotmail.com>
2020-12-29 15:33:29 +1100

    Added Pikachu and Eevee sound effects to play on mouseover

--- Day7: Split Landing Page/script.js
+++ Day7: Split Landing Page/script.js
@@ -1,9 +1,17 @@
 const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 const container = document.querySelector('.container');
+const pikachuSound = new Audio('./025-kanto-pikachu.mp3');
+const eeveeSound = new Audio('./eevee-starter.mp3');
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left');
+    pikachuSound.play();
+});
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
 
-right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+right.addEventListener('mouseenter', () => {
+    container.classList.add('hover-right');
+    eeveeSound.play();
+});
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
