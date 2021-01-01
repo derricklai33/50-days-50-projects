8e7503a41b9f206e7ebe0fe58e74ab1c64e08692
derricklai33 <derricklai33@hotmail.com>
2021-01-01 18:25:24 +1100

    Completed Day 9: Sound Board

--- Day9: Sound Board/script.js
+++ Day9: Sound Board/script.js
@@ -1,9 +1,23 @@
 const sounds = ['failling-down-stairs', 'omg-wow', "directed-by", "thomas-the-train", "sad-violin"];
 
 sounds.forEach(sound => {
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
 
     btn.innerText = sound;
 
+    btn.addEventListener('click', () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById('buttons').appendChild(btn);
 })
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+        song.pause();
+        song.currentTime = 0;
+    })
+}
